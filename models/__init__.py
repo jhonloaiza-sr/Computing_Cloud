4d128690bc432e9b9537ea0c4fbb2ee0c7fb912a
jhonloaiza-sr <jhon.loaiza@uao.edu.co>
2025-06-05 14:47:09 -0500

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Importa todos los modelos para que SQLAlchemy los detecte
-from .database import db
-from .user import User
-from .availability import Availability
-from .appointment import Appointment
-
-__all__ = ['db', 'User', 'Availability', 'Appointment']
